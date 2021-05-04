b72b0c80ecd32853167fad69b0b05678776ab1d3
Santiago8888 <santiago.marti67@gmail.com>
2021-05-03 21:53:15 -0500

    Tokenize words of every story.

--- scripts/pipelines.ts
+++ scripts/pipelines.ts
@@ -1,8 +1,30 @@
+import { iStoryFile, readStories } from '../../Cortazar/scripts/pipeline/reader'
+import { iRawStory } from '../../Cortazar/cortazar/src/types/stories'
+import { parseStories } from '../../Cortazar/scripts/pipeline/parser'
+import * as use from '@tensorflow-models/universal-sentence-encoder'
+import { tokenizeWords } from './functions'
 import { connect } from '../scripts/db'
 
 
-const compileDictionary = async() => {
+export const compileDictionary = async() => {
+    const files:iStoryFile[] = await readStories('../../Cortazar/scripts/data/stories', [])
+    let Words:string[] = []
+
+    for (const { payload } of files) {
+        const parsedStories:iRawStory[] = parseStories(payload.references)
+        const text:string = parsedStories.map(({title, subtitle, intro}) => 
+            `${title} ${subtitle} ${intro.join(' ')}`
+        ).join(' ')
+
+        const words = tokenizeWords(text)
+        Words = [...Words, ...words]
+    }
+
+
+    const model = await use.load()
+
     const { collection, client } = await connect('Dictionary')
+    
 
     await client.close()
 }
