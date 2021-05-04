ea4716e22c5552aa074895f31e7d3c3ad62a8503
Santiago8888 <santiago.marti67@gmail.com>
2021-05-03 22:36:00 -0500

    Embed top words & save to DB.

--- scripts/pipelines.ts
+++ scripts/pipelines.ts
@@ -1,9 +1,13 @@
 import { iStoryFile, readStories } from '../../Cortazar/scripts/pipeline/reader'
 import { iRawStory } from '../../Cortazar/cortazar/src/types/stories'
 import { parseStories } from '../../Cortazar/scripts/pipeline/parser'
+import PCA_Model from '../../Cortazar/cortazar/src/data/pca.json'
+
 import * as use from '@tensorflow-models/universal-sentence-encoder'
-import { tokenizeWords } from './functions'
-import { connect } from '../scripts/db'
+import { IPCAModel, PCA } from 'ml-pca'
+
+import { getCenter, tokenizeWords } from './functions'
+import { connect } from './db'
 
 
 export const compileDictionary = async() => {
@@ -20,12 +24,19 @@ export const compileDictionary = async() => {
         Words = [...Words, ...words]
     }
 
+    const wordMap = Words.reduce((d, i) => ({...d, [i]: d[i] ? d[i]+1 : 1 }), {}  as {[word:string]: number})
+    const wordFrequency = Object.entries(wordMap).map(([k,v])=> ({word:k, count:v}))
+    const sortedWords = wordFrequency.sort(({ count:a, count:b}) => a > b ? -1 : 1)
+    const topWords = sortedWords.filter((_, i) => i > 100 && i < 1000)
+    const dictionary = topWords.map(({ word }) => word)
 
     const model = await use.load()
+    const pca = PCA.load(PCA_Model as IPCAModel)
+    const wordEmbeddings = await getCenter(dictionary, {model, pca})
+    const wordDocuments = wordEmbeddings.map((word) => ({...word, frequency:wordMap[word.text]}))
 
     const { collection, client } = await connect('Dictionary')
-    
-
+    await collection.insertMany(wordDocuments)
     await client.close()
 }
 
