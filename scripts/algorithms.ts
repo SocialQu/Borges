3d2985e233739187060a7946acba520794a28bca
Santiago8888 <santiago.marti67@gmail.com>
2021-05-04 14:08:08 -0500

    Developed synonym finder.

--- scripts/algorithms.ts
+++ scripts/algorithms.ts
@@ -1,5 +1,32 @@
-const findSynonyms = (word:string):string[] => ['']
-const findAntonyms = (word:string):string[] => ['']
+import * as use from '@tensorflow-models/universal-sentence-encoder'
+import PCA_Model from '../../Cortazar/cortazar/src/data/pca.json'
+import { IPCAModel, PCA } from 'ml-pca'
+
+import { getCenter, sortBySimilarity } from './functions'
+import { iWordDoc } from './pipelines'
+import { connect } from './db'
+
+
+export const findSynonyms = async(word:string) => {
+    const model = await use.load()
+    const pca = PCA.load(PCA_Model as IPCAModel)
+    const [{center, embeddings}] = await getCenter([word], {model, pca})
+
+    const { collection, client } = await connect('Dictionary')
+    const geoNear = { $geoNear: { near:center, distanceField:'distance'}}
+    const limit = { $limit: 100 }
+    const pipeline = [ geoNear, limit ]
+    const synonyms:iWordDoc[] = await collection.aggregate(pipeline).toArray()
+
+    return sortBySimilarity(embeddings, synonyms)
+}
+
+
+
+const findAntonyms = (word:string):string[] => {
+    const sort = { $sort : { distance : -1 } }
+    return ['']
+}
 
 const findAnalogy = (analogy:[string, string], match:string):string[] => ['']
 const computeBias = (opposites:[string, string], word:string):number => 0
