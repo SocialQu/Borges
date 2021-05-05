5762be3d07dd875eea60a97d23fe86f340b1b34d
Santiago8888 <santiago.marti67@gmail.com>
2021-05-05 18:17:06 -0500

    Optimized types on Menu.

--- borges/src/components/layout/Menu.tsx
+++ borges/src/components/layout/Menu.tsx
@@ -10,59 +10,90 @@ const Lock = () => <img
 />
 
 interface iPosition { unit:number, module?:number, lesson?:number }
-interface iLesson { 
-    name:string
-    locked:boolean
+interface iMenuStyles {
+    lessonStyle:CSSProperties
+    lessonListStyle:CSSProperties
 
-    active:iPosition
-    position:iPosition
+    moduleStyle:CSSProperties
+    moduleListStyle:CSSProperties
 
-    style:CSSProperties
-    listStyle:CSSProperties
-
-    click(position:iPosition):void 
-}
-
-const Lesson = ({ name, locked, active, position, style, click }:iLesson) => {
-    const defaultListStyle = {lineHeight: 1.25}
-
-    return <li style={defaultListStyle} key={position.lesson}>
-        <a 
-            style={style} 
-            onClick={() => click(position)}
-            className={
-                active.unit === position.unit 
-                && active.module === position.module
-                && active.lesson === position.lesson
-                ? 'is-active': ''
-            }
-        > { locked && <Lock/> } { name } </a>
-    </li>
+    unitStyle:CSSProperties
+    unitListStyle:CSSProperties
 }
 
 
-interface iModule extends iLesson { lessons?:iLesson[], expanded:boolean }
-const Module = ({name, lessons=[], locked, style, listStyle, expanded, position, click}:iModule) => {
-    return <li style={{lineHeight:2, ...listStyle}} key={position.module}>
-        <a 
-            onClick={() => click(position)} 
-            style={locked ? {...style} : {cursor:'initial', ...style}}
-        >   { locked && <Lock/> } { name }  </a>
-
-        {   expanded && <ul> { lessons.map((l, i) => <Lesson {...l} /> )} </ul>   }
-    </li>
+interface iLesson { name:string, locked:boolean, active:boolean }
+interface iFullLesson { 
+    lesson: iLesson
+    position: iPosition
+    styles: iMenuStyles
+    navigate(position:iPosition):void 
 }
 
+const Lesson = ({ 
+    position, 
+    lesson:{ name, locked, active}, 
+    styles:{ lessonStyle, lessonListStyle }, 
+    navigate 
+}: iFullLesson) => <li style={{lineHeight: 1.25, ...lessonListStyle}}>
+    <a 
+        style={lessonStyle} 
+        onClick={() => navigate(position)}
+        className={ active ? 'is-active': '' }
+    > { locked && <Lock/> } { name } </a>
+</li>
 
-interface iUnit extends iLesson { modules?:iModule[] }
-const Unit = ({ modules = [] }:iUnit) => <>
-    <p className="menu-label"> Astroconsciencia </p>
+
+
+interface iModule extends iLesson { lessons:iLesson[] }
+interface iFullModule { 
+    module: iModule
+    expanded: boolean
+    position: iPosition
+    styles: iMenuStyles
+    navigate(position:iPosition):void 
+}
+
+const Module = ({
+    module:{name, active, locked, lessons }, 
+    expanded, 
+    position, 
+    styles: {moduleStyle, moduleListStyle}, 
+    navigate
+}:iFullModule) => <li style={{lineHeight:2, ...moduleListStyle}} key={position.module}>
+    <a 
+        onClick={() => navigate(position)} 
+        style={locked ? {...moduleStyle} : {cursor:'initial', ...moduleStyle}}
+    >   { locked && <Lock/> } { name }  </a>
+
+    {   expanded && <ul> { lessons.map((l) => <Lesson {...l} /> )} </ul>   }
+</li>
+
+
+
+const Unit = ({ name, modules = [], click }:iUnit) => <>
+    <a className="menu-label" onClick={() => click(position)}> { name } </a>
     <ul className="menu-list">
         { modules.map(m => <Module {...m} /> )}
     </ul>
 </>
 
-interface iMenu extends iLesson { units:iUnit[], menuStyle:CSSProperties, navigate(position:iPosition):void }
+
+
+interface iUnit extends iModule { modules:iModule[] }
+interface iMenu {
+    units: iUnit[]
+    activeItem?: iPosition
+
+    lessonStyle?: CSSProperties
+    listLessonStyle?: CSSProperties
+    moduleStyle?: CSSProperties
+    listModuleStyle?: CSSProperties
+    menuStyle?: CSSProperties
+
+    navigate(position:iPosition): void
+}
+
 export const Menu = ({ units, menuStyle, navigate }: iMenu) => {
     const defaultStyle = { minHeight:'calc(100vh - 85px)', width:250, boxShadow: '3px 0 3px 0 #ccc', fontSize:'1.15em' }
 
