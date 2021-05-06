76138640a78dc7de0e52de638dfc947fac2fc97a
Santiago8888 <santiago.marti67@gmail.com>
2021-05-06 13:11:24 -0500

    Export new atoms from index.

--- borges/src/components/atoms/index.tsx
+++ borges/src/components/atoms/index.tsx
@@ -1,2 +1,4 @@
 export { Text, Title, Subtitle, Paragraph } from './Text'
-export { CTA, Submit, Button, ButtonBox} from './Button'
+export { CTA, Submit, Button, ButtonBox } from './Button'
+export { Chart } from './Chart'
+export { Media } from './Media'
