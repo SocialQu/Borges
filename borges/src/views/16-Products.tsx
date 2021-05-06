67b212952daf02d9d3438ada81699a429107b96c
Santiago8888 <santiago.marti67@gmail.com>
2021-05-06 15:42:39 -0500

    Products Lesson Content.

---
+++ borges/src/views/16-Products.tsx
@@ -0,0 +1,17 @@
+import { Lesson } from "../components/cells/Lesson"
+import { Grid } from '../components/molecules/Grid'
+
+const title = 'Products'
+export const Products = () => <Lesson title={title} next={() => {}}>
+    <p>
+        This is a list of products, SaaS and startups that use word embeddings to create innovative products. 
+
+        And if you try a product, do not forget to rate it and leave an encouragement comment.
+
+        If you would like to submit a product you created or use please use this form. 
+    </p>
+
+    <Grid items={['Cortazar', 'Pegasus Summarization']}/>
+
+
+</Lesson>
