ce369fbd4dbf77fc365f185a7a1cd7eb7b3ac817
Santiago8888 <santiago.marti67@gmail.com>
2021-05-06 13:10:18 -0500

    Form elements placeholder.

---
+++ borges/src/components/molecules/Form.tsx
@@ -0,0 +1,6 @@
+interface iInputForm { label:string } 
+export const InputForm = ({label}:iInputForm) => <> { label } </>
+
+export const TextAreaForm = ({label}:iInputForm) => <> { label } </>
+
+export const Form = ({label}:iInputForm) => <> { label } </>
