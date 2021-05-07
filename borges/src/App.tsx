d222a2ec2d4b97e9f6f1caa920ecf0a8c2ed2d5d
Santiago8888 <santiago.marti67@gmail.com>
2021-05-06 20:24:49 -0500

    Declared units and internal state.

--- borges/src/App.tsx
+++ borges/src/App.tsx
@@ -1,14 +1,41 @@
+import { Menu, iUnit, iPosition } from './components/layout/Menu'
 import { NavBar } from './components/layout/NavBar'
-import { Menu } from './components/layout/Menu'
-
 import { Home } from './views/Home'
 
+import { useState } from 'react'
 import './App.css'
 
-export const App = () => <div className="App">
-	<NavBar />
-	<Menu units={[]} navigate={() => {}}/>
+const Units:iUnit[] = [{ 
+		name:'Word Embeddings', modules:[
+			{ name:'Introduction', locked:true },
+			{ name:'What are word embeddings?', locked:true },
+			{ name:'Application: Finding Synonyms', locked:true },
+			{ name:'Topic Classification', locked:true },
+			{ name:'How to train Word Embeddings?', locked:true },
+			{ name:'Tokenization', locked:true },
+			{ name:'Co-ocurrence Matrix', locked:true },
+			{ name:'Dimensionality Reduction', locked:true },
+			{ name:'Application: Solving the Analogy', locked:true },
+			{ name:'Application: Detecting Biasis', locked:true },
+			{ name:'Quiz', locked:true },
+			{ name:'Best Answers', locked:true },
+			{ name:'Wall of Fame', locked:true },
+			{ name:'Advanced Topics', locked:true },
+			{ name:'Addditional Resources', locked:true },
+			{ name:'Startups', locked:true },
+			{ name:'Next Steps', locked:true },
+			{ name: 'References', locked:true }
+		], 
+}, { name:'Sentiment Analysis', modules:[] }]
 
-	<Home lesson={0}/>
 
-</div>
+export const App = () => {
+	const [ units, setUnits ] = useState(Units)
+	const [ position, setPosition ] = useState<iPosition>({ unit:0, module:0 })
+
+	return <div className="App">
+		<NavBar />
+		<Menu units={[]} navigate={(position) => setPosition(position)}/>
+		<Home position={position}/>
+	</div>
+}
