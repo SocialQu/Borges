06836382294c157760a5266f0327b7651c5ba8aa
Santiago8888 <santiago.marti67@gmail.com>
2021-05-07 15:19:36 -0500

    Inline Input Form Defined.

--- borges/src/components/molecules/Form.tsx
+++ borges/src/components/molecules/Form.tsx
@@ -1,6 +1,34 @@
-interface iInputForm { label:string } 
-export const InputForm = ({label}:iInputForm) => <> { label } </>
+import { useState } from "react"
 
-export const TextAreaForm = ({label}:iInputForm) => <> { label } </>
+interface iInputForm { 
+    placeholder:string
+    submit(value:string):void
+} 
 
-export const Form = ({label}:iInputForm) => <> { label } </>
+export const InputForm = ({placeholder, submit}:iInputForm) => {
+    const [ value, setValue ] = useState('')
+
+    return <div className="field has-addons">
+        <div className="control">
+            <input 
+                type="text" 
+                value={value}
+                className="input" 
+                placeholder={placeholder} 
+                onChange={({target:{value}}) => setValue(value)}
+            />
+        </div>
+
+        <div className="control">
+            <a 
+                className="button is-info" 
+                onClick={() => submit(value)}
+            > Submit </a>
+        </div>
+    </div>
+}
+
+
+interface iForm { label:string }
+export const TextAreaForm = ({label}:iForm) => <> { label } </>
+export const Form = ({label}:iForm) => <> { label } </>
