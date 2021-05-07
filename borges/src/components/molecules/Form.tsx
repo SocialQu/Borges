bbe6e65c30d98be5c60bac5d1a9f7aeb42d948e4
Santiago8888 <santiago.marti67@gmail.com>
2021-05-07 16:01:24 -0500

    TextArea From Component.

--- borges/src/components/molecules/Form.tsx
+++ borges/src/components/molecules/Form.tsx
@@ -29,6 +29,34 @@ export const InputForm = ({placeholder, submit}:iInputForm) => {
 }
 
 
+interface iTextAreaForm { label:string, submit(value:string):void }
+export const TextAreaForm = ({label, submit}:iTextAreaForm) => {
+    const [value, setValue] = useState('')
+
+
+    return <div>
+        <div className="field">
+            <label className="label"> {label} </label>
+            <div className="control">
+                <textarea 
+                    rows={10} 
+                    value={value}
+                    className="textarea" 
+                    placeholder="10 lines of textarea" 
+                    onChange={({target:{value}}) => setValue(value)}
+                />
+            </div>
+        </div>
+
+        <div className="control">
+            <button 
+                className="button is-link"
+                onClick={() => submit(value)}
+            >Submit</button>
+        </div>
+    </div>
+}
+
+
 interface iForm { label:string }
-export const TextAreaForm = ({label}:iForm) => <> { label } </>
 export const Form = ({label}:iForm) => <> { label } </>
