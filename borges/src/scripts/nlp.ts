448c35bff36d87c39450d914dd8876756128c27d
Santiago8888 <santiago.marti67@gmail.com>
2021-05-07 16:19:14 -0500

    Classify Text Algorithm.

--- borges/src/scripts/nlp.ts
+++ borges/src/scripts/nlp.ts
@@ -1,26 +1,28 @@
+import { sortBySimilarity, tokenizeSentences } from './utils'
 import { iWordDoc } from '../types/db'
 import { iModels } from '../types/ai'
 import { getCenter } from './utils'
 import { User } from 'realm-web'
 
 
-const similarity = (center:number[], embedding: number[]) => {
-    if (center.length !== embedding.length) return Infinity
-    const delta = center.reduce((d, i, idx) => d + Math.abs(i - embedding[idx]), 0)
-    return delta
-}
-
-const sortBySimilarity = (center:number[], synonymns:{embeddings:number[]}[]) => synonymns.sort(({embeddings:a}, {embeddings:b}) => 
-    similarity(center, a) > similarity(center, b) ? 1 : -1
-)
-
-
 interface iFindSynonyms { word:string, user:User, models:iModels}
 interface iSynonyms { word:string, center:[number, number], embeddings:number[]} 
 export const findSynonyms = async({word, user, models:{pca, model}}: iFindSynonyms):Promise<iSynonyms[]> => {
     const [{center, embeddings}] = await getCenter([word], {model, pca})
-    const matches:iWordDoc[] = await user.functions.borgesSynonyms(center)
+    const matches:iWordDoc[] = await user.functions.borgesFindSynonyms(center)
 
     const synonyms =  sortBySimilarity(embeddings, matches)
     return synonyms as iSynonyms[]
+}
+
+
+interface iFindiTopics { text:string, user:User, models:iModels}
+interface iTopic { topic:string, center:[number, number], embeddings:number[]} 
+export const classifyText = async({text, models:{pca, model}, user}: iFindiTopics):Promise<iTopic[]> => {
+    const sentences = tokenizeSentences(text)
+    const [{center, embeddings}] = await getCenter(sentences, {model, pca})
+    const matches:iTopic[] = await user.functions.borgesFindSynonyms(center)
+
+    const topics =  sortBySimilarity(embeddings, matches)
+    return topics as iTopic[]
 }
