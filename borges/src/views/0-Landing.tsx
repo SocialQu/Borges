38921e1129e514d4b7d219a0b5cbfabc9b8f1ed3
Santiago8888 <santiago.marti67@gmail.com>
2021-05-06 19:57:15 -0500

    Landing with Video.

--- borges/src/views/0-Landing.tsx
+++ borges/src/views/0-Landing.tsx
@@ -1,6 +1,10 @@
 import { Landing } from '../components/cells/Landing'
+import YouTubePlayer from 'react-player/youtube'
+
 
 const title = 'Introduction to Word Embeddings'
 const subtitle = 'Gain an intuitive understanding of the AI technology that powers human language understanding.'
 
-export const BorgesLanding = () => <Landing title={title} subtitle={subtitle} cta={() => {}} />
+export const BorgesLanding = () => <Landing title={title} subtitle={subtitle} cta={() => {}}>
+    <YouTubePlayer url={'https://www.youtube.com/watch?v=ZytU3s1T2XY'} />
+</Landing>
