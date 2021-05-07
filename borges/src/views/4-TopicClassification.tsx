2662adc1505907cb99b18fc084fa08b15d3bf7f2
Santiago8888 <santiago.marti67@gmail.com>
2021-05-07 16:03:57 -0500

    Scatter and Form for Topic Classification.

--- borges/src/views/4-TopicClassification.tsx
+++ borges/src/views/4-TopicClassification.tsx
@@ -1,13 +1,15 @@
 import { TextAreaForm } from '../components/molecules/Form'
 import { Lesson } from "../components/cells/Lesson"
-import { Chart } from '../components/atoms'
+import { Scatter } from '../components/atoms/Chart'
+import { Subtitle } from '../components/atoms'
 
 const title = 'Topic Classification'
 interface iTopicClassification {next():void}
 export const TopicClassification = ({next}:iTopicClassification) => <Lesson title={title} next={next}>
     <p> This is chart that maps the average word embeddings for texts in different topics:</p>
 
-    <Chart title="Topic's Position" />
+    <Subtitle text="Topic's Position"/>
+    <Scatter data={[{name:'Startups', x:1, y:2}, {name:'VC', x:3, y:4}]} />
 
     <p>
         The process of finding the topic of a text, sentence or even a word is very similar to finding the synonyms. 
@@ -15,7 +17,7 @@ export const TopicClassification = ({next}:iTopicClassification) => <Lesson titl
         Try it out, input a text or sentence and find the likelihood the text find a topic.
     </p>
 
-    <TextAreaForm label="Classify a Text"/>
+    <TextAreaForm label="Classify a Text" submit={() => {}}/>
 
     <p>
         This process can be extrapolated to other tasks like sentiment analysis, and it works with labeled data. 
