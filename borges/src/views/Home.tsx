6b541178764ca8d31f94352b7bd87929a26ceb78
Santiago8888 <santiago.marti67@gmail.com>
2021-05-06 20:20:22 -0500

    Home with position property.

--- borges/src/views/Home.tsx
+++ borges/src/views/Home.tsx
@@ -1,3 +1,6 @@
+import { iPosition } from '../components/layout/Menu'
+import { SentimentAnalysis } from './SentimentAnalysis'
+
 import { BorgesLanding } from './0-Landing'
 import { Introduction } from './1-Introduction'
 import { WordEmbeddings } from './2-WordEmbeddings'
@@ -19,25 +22,28 @@ import { NextSteps } from './17-NextSteps'
 import { References } from './18-References'
 
 
-export const Home = ({ lesson }: { lesson:number }) => {
-    if(lesson === 1) return <Introduction />
-    if(lesson === 2) return <WordEmbeddings />
-    if(lesson === 3) return <Synonyms />
-    if(lesson === 4) return <TopicClassification />
-    if(lesson === 5) return <Training />
-    if(lesson === 6) return <Tokenization />
-    if(lesson === 7) return <CoOcurrenceMatrix />
-    if(lesson === 8) return <DimensionalityReduction />
-    if(lesson === 9) return <Analogies />
-    if(lesson === 10) return <Biasis />
-    if(lesson === 11) return <WordEmbeddingsQuiz />
-    if(lesson === 12) return <BestAnswers />
-    if(lesson === 13) return <WallOfFame />
-    if(lesson === 14) return <AdvancedTopics />
-    if(lesson === 15) return <WordEmbeddingsMedia />
-    if(lesson === 16) return <Products />
-    if(lesson === 17) return <NextSteps />
-    if(lesson === 18) return <References />
+
+export const Home = ({ position: { unit, module } }: { position:iPosition }) => {
+    if(unit === 1) return <SentimentAnalysis />
+
+    if(module === 0) return <Introduction />
+    if(module === 1) return <WordEmbeddings />
+    if(module === 2) return <Synonyms />
+    if(module === 3) return <TopicClassification />
+    if(module === 4) return <Training />
+    if(module === 5) return <Tokenization />
+    if(module === 6) return <CoOcurrenceMatrix />
+    if(module === 7) return <DimensionalityReduction />
+    if(module === 8) return <Analogies />
+    if(module === 9) return <Biasis />
+    if(module === 10) return <WordEmbeddingsQuiz />
+    if(module === 11) return <BestAnswers />
+    if(module === 12) return <WallOfFame />
+    if(module === 13) return <AdvancedTopics />
+    if(module === 14) return <WordEmbeddingsMedia />
+    if(module === 15) return <Products />
+    if(module === 16) return <NextSteps />
+    if(module === 17) return <References />
 
     return <BorgesLanding />
 }
