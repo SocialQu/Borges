7da308261d519c2ac4143f75823e36c2766e422c
Santiago8888 <santiago.marti67@gmail.com>
2021-05-09 21:32:24 -0500

    Fixed return type on tokenizer.

--- scripts/functions.ts
+++ scripts/functions.ts
@@ -16,8 +16,10 @@ export interface iModels { model:UniversalSentenceEncoder, pca:PCA }
 interface iWordEmbedding { text:string, embeddings:number[], center:number[] }
 export const getCenter = async(text:string[], {model, pca}:iModels):Promise<iWordEmbedding[]> => {
     const tensor = await model.embed(text)
+
     const embeddings = await tensor.array()
     const center = pca.predict(embeddings, {nComponents:2}).to2DArray()
+
     const wordEmbeddings = text.map((t, i) => ({ text:t, embeddings:embeddings[i], center:center[i]}))
     return wordEmbeddings
 }
@@ -27,5 +29,5 @@ export const sortBySimilarity = (center:number[], synonymns:{embeddings:number[]
     similarity(center, a) > similarity(center, b) ? 1 : -1
 )
 
-export const tokenizeWords = (text:string) => text.match(/(\b[^ $]+\b)/g)
+export const tokenizeWords = (text:string):string[] => text.match(/(\b[^ $]+\b)/g) as string[]
 export const tokenizeSentences = (text:string) => text.match( /[^\.!\?]+[\.!\?]+/g )
