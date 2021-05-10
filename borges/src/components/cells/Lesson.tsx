f51a5a362cbbde9187f4a13ba507df5a5cbac736
Santiago8888 <santiago.marti67@gmail.com>
2021-05-10 16:44:26 -0500

    Styled Lesson.

--- borges/src/components/cells/Lesson.tsx
+++ borges/src/components/cells/Lesson.tsx
@@ -1,8 +1,24 @@
-import { Title, Button } from '../atoms'
+import { CSSProperties } from 'react'
+import { Title, Submit } from '../atoms'
 
-interface iLesson {title:string, children?: JSX.Element | JSX.Element[], next():void}
-export const Lesson = ({ title, children, next }:iLesson) => <div className="content">
-    <Title text={title} />
+interface iLesson {
+    title:string
+    style?: CSSProperties
+    children?: JSX.Element | JSX.Element[]
+    next():void
+}
+
+const defaultStyle:CSSProperties = {
+    margin:'auto',
+    maxWidth:720,
+    fontSize:21
+}
+
+export const Lesson = ({ title, style, children, next }:iLesson) => <div 
+    className="content" 
+    style={{...defaultStyle, ...style}}
+>
+    <Title text={title} style={{marginBottom:'2rem'}}/>
     { children }
-    <Button click={next} />
+    <Submit click={next} text={'Next'} buttonClass={'is-link'} style={{marginTop:'2rem'}}/>
 </div>
