c063c203f819d3672679fdc4dfc9f863be1a5e7e
Santiago8888 <santiago.marti67@gmail.com>
2021-05-10 15:47:02 -0500

    Style Landing Component.

--- borges/src/components/cells/Landing.tsx
+++ borges/src/components/cells/Landing.tsx
@@ -1,9 +1,12 @@
 import { Title, Subtitle, CTA } from '../atoms'
+import { CSSProperties } from 'react'
 
 interface iLanding {title:string, subtitle:string, children?: JSX.Element | JSX.Element[], cta():void}
+const subtitleStyle:CSSProperties = { maxWidth:640, margin:'0px auto 3rem' }
+const buttonStyle:CSSProperties = { paddingLeft:64, paddingRight:64, marginTop: '2.5rem' }
 export const Landing = ({ title, subtitle, children, cta }:iLanding) => <div className="content">
     <Title text={title} />
-    <Subtitle text={subtitle}/>
+    <Subtitle text={subtitle} style={subtitleStyle}/>
     { children }
-    <CTA click={cta} />
+    <CTA click={cta} text={'Start Learning'} buttonClass={'is-link'} style={buttonStyle}/>
 </div>
