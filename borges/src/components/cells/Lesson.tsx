53e13b693bac5345ec74ba3535dbfeb9295c8c2e
Santiago8888 <santiago.marti67@gmail.com>
2021-05-11 05:11:28 -0500

    Lesson, accept title style.

--- borges/src/components/cells/Lesson.tsx
+++ borges/src/components/cells/Lesson.tsx
@@ -4,6 +4,7 @@ import { Title, Submit } from '../atoms'
 interface iLesson {
     title:string
     style?: CSSProperties
+    titleStyle?: CSSProperties
     children?: JSX.Element | JSX.Element[]
     next():void
 }
@@ -22,11 +23,11 @@ const buttonStyle:CSSProperties = {
     paddingRight:48
 }
 
-export const Lesson = ({ title, style, children, next }:iLesson) => <div 
+export const Lesson = ({ title, style, titleStyle, children, next }:iLesson) => <div 
     className="content" 
     style={{...defaultStyle, ...style}}
 >
-    <Title text={title} style={{marginBottom:'2rem', color:'chocolate'}}/>
+    <Title text={title} style={{marginBottom:'2rem', color:'chocolate', ...titleStyle}}/>
     <div style={{textAlign:'left'}}> { children } </div>
     <Submit click={next} text={'Next'} buttonClass={'is-link'} style={buttonStyle}/>
 </div>
