32af1d7e0a46507d393b3d80a2dd7e7637fbbf9e
Santiago8888 <santiago.marti67@gmail.com>
2021-05-11 06:22:28 -0500

    Minor code formatting on Landing.

--- borges/src/components/cells/Landing.tsx
+++ borges/src/components/cells/Landing.tsx
@@ -5,7 +5,7 @@ interface iLanding {title:string, subtitle:string, children?: JSX.Element | JSX.
 const subtitleStyle:CSSProperties = { maxWidth:660, margin:'0px auto 3rem', color:'#454545', fontSize:'2rem' }
 const buttonStyle:CSSProperties = { paddingLeft:64, paddingRight:64, marginTop:'2em', backgroundColor:'midnightblue' }
 export const Landing = ({ title, subtitle, children, cta }:iLanding) => <div className="content">
-    <Title text={title} style={{color:'chocolate',     marginBottom:'0.5rem'}} />
+    <Title text={title} style={{color:'chocolate', marginBottom:'0.5rem'}} />
     <Subtitle text={subtitle} style={subtitleStyle}/>
     { children }
     <CTA click={cta} text={'Start Learning'} buttonClass={'is-link'} style={buttonStyle}/>
