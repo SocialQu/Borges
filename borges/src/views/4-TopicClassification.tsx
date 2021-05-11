7c2dd41e75b4111bdc4f2589cdd7ea6af0670384
Santiago8888 <santiago.marti67@gmail.com>
2021-05-10 20:51:11 -0500

    Add subtitle and styled lesson 4 footer.

--- borges/src/views/4-TopicClassification.tsx
+++ borges/src/views/4-TopicClassification.tsx
@@ -30,12 +30,13 @@ export const TopicClassification = ({next, models, user}:iTopicClassification) =
             Try it out, input a text or sentence and find the likelihood the text find a topic.
         </p>
 
-        <TextAreaForm label="Classify a Text" submit={getTopics}/>
+        <Subtitle text="Excersice: Classify a Text"/>
+        <TextAreaForm label="" submit={getTopics} placeholder={'Drop a text or sentence to find its related topics.'}/>
         <ul> { topics?.map((topic)=> <li> { topic } </li> ) } </ul>
 
         <p>
             This process can be extrapolated to other tasks like sentiment analysis, and it works with labeled data. 
-            <strong>Question:</strong> How would you classify text using unsupervised learning?
+            <strong> Reflection:</strong> <i>How would you classify texts using unsupervised learning (without unlabeled data)?</i>
         </p>
     </Lesson>
 }
