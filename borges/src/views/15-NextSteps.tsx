c07fb88bd0a696993d0150b5cd014ee4eec54a57
Santiago8888 <santiago.marti67@gmail.com>
2021-05-12 20:56:48 -0500

    Add links to next steps.

--- borges/src/views/15-NextSteps.tsx
+++ borges/src/views/15-NextSteps.tsx
@@ -11,11 +11,12 @@ export const NextSteps = ({next}:iNextSteps) => <Lesson title={title} next={next
     <p>
         Next month, I will release the a paid module about Sentiment Analysis, 
         it will be based on lectures 5-8 of Stanford's CS224U. 
-        You can visit the <a>pre-sale product</a> site and enjoy an early bird discount. 
+        You can visit the <a href={"https://gum.co/nlp-sentiment-analysis"}>pre-sale product</a> 
+        site and enjoy an early bird discount. 
         Plus support me in my mission of sharing and educating future NLP engineers.
     </p>
 
     <p>
-        If you want to collaborate you can connect with me on <a>LinkedIn</a>.
+        For more content of my content, you can visit my <a href={"https://santiagoq.medium.com/"}>Medium</a> blog.
     </p>
 </Lesson>
