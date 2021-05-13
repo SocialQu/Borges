a4d33ebe3fe94208321674c19bd593a3a0526b36
Santiago8888 <santiago.marti67@gmail.com>
2021-05-12 20:24:37 -0500

    Edited content for the co-ocurrence matrix lesson.

--- borges/src/views/7-CocurrenceMatrix.tsx
+++ borges/src/views/7-CocurrenceMatrix.tsx
@@ -1,41 +1,60 @@
 import { Lesson } from "../components/cells/Lesson"
 import { Subtitle } from '../components/atoms'
 
-const coocurrenceMatrixImg = "https://www.researchgate.net/profile/Majid_F_Sadi/publication/332703770/figure/fig1/AS:752289044234240@1556371093356/1-Co-occurrence-matrix-for-three-sample-sentences.ppm" 
-const title = 'Co-ocurrence Matrix'
-interface iCoOcurrenceMatrix {words:string[], matrix:number[][], next():void}
-export const CoOcurrenceMatrix = ({words, matrix, next}:iCoOcurrenceMatrix) => <Lesson title={title} next={next}>
+const coOccurrenceMatrixImg = "https://www.researchgate.net/profile/Majid_F_Sadi/publication/332703770/figure/fig1/AS:752289044234240@1556371093356/1-Co-occurrence-matrix-for-three-sample-sentences.ppm" 
+const title = 'Co-occurrence Matrix'
+interface iCooccurrenceMatrix {words:string[], matrix:number[][], next():void}
+export const CooccurrenceMatrix = ({words, matrix, next}:iCooccurrenceMatrix) => <Lesson title={title} next={next}>
     <p>
-        The second step involves finding adyacent words, and mapping this relationships in a Matrix. 
-        The first step is to enlist every unique word in our corpus (text dataset). 
-        Once defined, we will build a square matrix where each column and row represents a word.
+        The co-occurrence matrix contains the frequency that two different words occur next to each other. 
+        Building the matrix is a four-step process:
     </p>
 
-    <Subtitle text="Example: Co-ocurrence Matrix" style={{textAlign:'center', marginTop:'2rem'}}/>
-    <img src={coocurrenceMatrixImg}/>
+    <ol style={{marginLeft:24, marginTop:'0.5rem'}}>
+        <li> Create a set with every unique word in the training dataset (corpus). </li>
+        <li> Create a squared matrix where each row and column represents a word. </li>
+        <li> Count the occurrences of neighbor words based on an N-word window. </li>
+        <li> Insert the count to the corresponding cell in the matrix. </li>
+    </ol>
 
-    <p>
-        Next, define an N-sized window, where every word that is at most N words appart is considered adjacent like this:
-    </p>
+    <hr style={{height:3, margin: '2em auto', maxWidth: 600 }}/>
 
-    <Subtitle text="Example: Building a Co-ocurrence Matrix" style={{textAlign:'center', marginTop:'2rem'}}/>
-    <div className={"code"}>
-        // For an 3 sized window: <br/>
-        const str = 'I enjoy learning about Natural Language Proccesing.' <br/><br/>
+    <Subtitle text="Example: A Co-occurrence Matrix" style={{textAlign:'center', marginTop:'2rem'}}/>
+    <img src={coOccurrenceMatrixImg}/>
 
-        // enjoy &amp; learning are adyacent. <br/>
-        // enjoy &amp; about are adyacent. <br/>
-        // enjoy &amp; Natural are adyacent. <br/>
-        // enjoy &amp; Language are *NOT* adyacent. <br/>
+    <div className="code" style={{marginLeft:0, marginRight:0}}>
+        // Sentences used to build the samle co-occurrence matrix. <br/>
+        1. I like deep learning. <br/>
+        2. I enjoy flying. <br/>
+        3. I like NLP. <br/>
     </div>
 
+    <hr style={{height:3, margin: '2em auto', maxWidth: 600 }}/>
+
     <p>
-        For our case we are going to use a 5 word window and based on the text you submitted, 
-        this is how your co-occurrence matrix looks:
+        The above example worked with a 1-word-window. 
+        To illustrate how this differs based on the window size, consider the following sentence:
+    </p>
+
+    <div className={"code"}>
+        // For an 3-word-window: <br/>
+        const str = 'I enjoy learning Natural Language Processing.' <br/><br/>
+
+        // enjoy &amp; learning are adyacent. <br/>
+        // enjoy &amp; Natural are adyacent. <br/>
+        // enjoy &amp; Language are adyacent. <br/>
+        // enjoy &amp; Processing are *NOT* adyacent. <br/>
+    </div>
+
+    <hr style={{height:3, margin: '2em auto', maxWidth: 600 }}/>
+
+    <p>
+        For our word embeddings, we are going to use a 5-word-window. <br/>
+        This is your co-occurrence matrix based on the text you submitted in the previous lesson:
     </p>
 
     <div className="table-container">
-        <Subtitle text="Your Co-ocurrence Matrix" style={{textAlign:'center', marginTop:'2rem'}}/>
+        <Subtitle text="Your Co-occurrence Matrix" style={{textAlign:'center', marginTop:'2rem'}}/>
         <table className="table">
             <thead>
                 <tr> 
