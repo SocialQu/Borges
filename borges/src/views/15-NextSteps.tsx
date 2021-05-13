53da86ac2325cd50a30fd1db079c348013a6826c
Santiago8888 <santiago.marti67@gmail.com>
2021-05-13 15:57:50 -0500

    Next steps content edited.

--- borges/src/views/15-NextSteps.tsx
+++ borges/src/views/15-NextSteps.tsx
@@ -4,19 +4,21 @@ const title = 'Next Steps'
 interface iNextSteps {next():void}
 export const NextSteps = ({next}:iNextSteps) => <Lesson title={title} next={next}>
     <p>
-        Congratulations! You reached the end, if you want to deeper you can visit the Stanford Courses. 
-        A number of free are also available to download for free:
+            Congratulations! You reached the end of this module, 
+            if you want to deeper you can visit Stanford's CS224U &#38; CS224U Course and Video Lectures.
     </p>
 
     <p>
-        Next month, I will release the a paid module about Sentiment Analysis, 
-        it will be based on lectures 5-8 of Stanford's CS224U. 
-        You can visit the <a href={"https://gum.co/nlp-sentiment-analysis"}>pre-sale product</a> 
-        site and enjoy an early bird discount. 
-        Plus support me in my mission of sharing and educating future NLP engineers.
+        Next month (mid-June), I plan to release a second module about Sentiment Analysis. 
+        It will be based on lectures 5-8 from Stanford's CS224U course. 
+
+        You can visit the <a href={"https://gum.co/nlp-sentiment-analysis"}>product page,</a> 
+        to express interest, subscribe to the waiting, or contribute to development costs.
+
+        It will be served with a pay-as-you-want model.
     </p>
 
     <p>
-        For more content of my content, you can visit my <a href={"https://santiagoq.medium.com/"}>Medium</a> blog.
+        For more content of my content, you can visit my <a href={"https://santiagoq.medium.com/"}>blog</a>.
     </p>
 </Lesson>
