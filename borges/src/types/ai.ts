fdbb29f787346a862f68ce966f7025cbf01f1183
Santiago8888 <santiago.marti67@gmail.com>
2021-05-14 15:06:15 -0500

    Extend main models interface with 2 pca models.

--- borges/src/types/ai.ts
+++ borges/src/types/ai.ts
@@ -1,4 +1,4 @@
 import { UniversalSentenceEncoder } from '@tensorflow-models/universal-sentence-encoder'
 import { PCA } from 'ml-pca'
 
-export interface iModels { model:UniversalSentenceEncoder, pca:PCA }
+export interface iModels { model:UniversalSentenceEncoder, topicsPCA:PCA, wordsPCA:PCA }
