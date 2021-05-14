31813b9cc41a778166f17308dd0903d482b3aadc
Santiago8888 <santiago.marti67@gmail.com>
2021-05-14 15:06:27 -0500

    Topic Doc interface mapped.

--- borges/src/types/db.ts
+++ borges/src/types/db.ts
@@ -1,1 +1,2 @@
 export interface iWordDoc {word:string, embeddings:number[], center:number[], frequency:number}
+export interface iTopicDoc { topic:string, center:[number, number], embeddings:number[]} 
