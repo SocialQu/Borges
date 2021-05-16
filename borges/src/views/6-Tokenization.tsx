a5b37d3bb6bdf8b4c51af17a58f34d28db97b7d1
Santiago8888 <santiago.marti67@gmail.com>
2021-05-16 03:15:35 -0500

    Emphasize tokenization lesson.

--- borges/src/views/6-Tokenization.tsx
+++ borges/src/views/6-Tokenization.tsx
@@ -14,16 +14,15 @@ const title = 'Tokenization'
 interface iTokenization {embeddings:iEmbeddings[], getWords(text:string):void, next():void}
 export const Tokenization = ({embeddings, getWords, next}:iTokenization) => <Lesson title={title} next={next}>
     <p>
-        There is a long tradition in Natural Language Processing (NLP) to separate words that includes stemming and lemmatization. 
-        To exemplify some of the difficulties we need to consider when splitting a text, consider the following[1]:
+        There is a long tradition in Natural Language Processing (NLP) to <strong>separate words</strong> that includes stemming and lemmatization.<br/><br/> Some of the <strong>difficulties</strong> when splitting a text include:
     </p>
 
     <ul style={{marginBottom:'2rem'}}>
-        <li> Words that have the same meaning, including plurals and conjugated verbs. </li>
+        <li> Words that have the <strong>same meaning</strong>, including plurals and conjugated verbs. </li>
         <li> Pronouns, prepositions, and articles that appear frequently but contribute little additional meaning. </li>
-        <li> Abbreviations and compound words like N.Y.C. or New York. </li>
+        <li> <strong>Abbreviations</strong> and compound words like N.Y.C. or New York. </li>
         <li> Words with internal hyphens or apostrophes. </li>
-        <li> Numbers, symbols, and punctuation signs like parenthesis or ellipsis. </li>
+        <li> Numbers, symbols, and <strong>punctuation signs</strong> like parenthesis or ellipsis. </li>
         <li> Orthographic errors. </li>
     </ul>
 
@@ -33,15 +32,15 @@ export const Tokenization = ({embeddings, getWords, next}:iTokenization) => <Les
     </SyntaxHighlighter>
 
     <p>
-        With the advent of Deep Learning, tokenization has partially lost relevance. Because theoretically, 
-        the best AI models should handle irregularities by themselves, and building tokenizers is a slow, 
+        With the advent of Deep Learning, tokenization has partially lost relevance. Because 
+        <strong> the best AI models should handle irregularities</strong> by themselves, and building tokenizers is a slow, 
         manual process. However, using tokenizers or other input transformations can dramatically reduce training speed and improve accuracy.    
     </p>
 
     <hr style={{height:3, margin: '2em auto', maxWidth: 600 }}/>
 
     <p>
-        To start training your own word embeddings, please insert a short text (~20 words) to tokenize it:
+        To start training your own word embeddings, please <strong>insert a short text</strong> (~20 words) to tokenize it:
     </p>
 
 
@@ -50,6 +49,6 @@ export const Tokenization = ({embeddings, getWords, next}:iTokenization) => <Les
     <hr style={{height:3, margin: '2em auto', maxWidth: 600 }}/>
 
     <p>
-        <strong> Important: </strong> the text you submitted will be used in the next 2 lessons.
+        <strong> Important: </strong> tha text you submitted will be used in the next 2 lessons.
     </p>
 </Lesson>
